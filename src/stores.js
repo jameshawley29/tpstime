fd3a0364477e3e8d2a54fa857045a28f3212c621
jameshawley29 <jameshawley29@gmail.com>
2023-02-20 20:46:09 -0500

    Changed Day to B

--- src/stores.js
+++ src/stores.js
@@ -60,6 +60,65 @@ export let periods = [
   ]
 */
 
+/*[
+  {
+    "name": "Advisory",
+    "time": [
+      "08:00",
+      "08:20"
+    ]
+  },
+  {
+    "name": "2nd Period",
+    "time": [
+      "08:25",
+      "09:45"
+    ]
+  },
+  {
+    "name": "Break/Flex",
+    "time": [
+      "09:50",
+      "10:15"
+    ]
+  },
+  {
+    "name": "4th Period",
+    "time": [
+      "10:20",
+      "11:40"
+    ]
+  },
+  {
+    "name": "US Middle Block",
+    "time": [
+      "11:45",
+      "12:35"
+    ]
+  },
+  {
+    "name": "US Lunch",
+    "time": [
+      "12:40",
+      "13:20"
+    ]
+  },
+  {
+    "name": "6th Period",
+    "time": [
+      "13:25",
+      "14:45"
+    ]
+  },
+  {
+    "name": "Study Period",
+    "time": [
+      "14:45",
+      "15:15"
+    ]
+  }
+]*/
+
 export let periods = [
   {
     "name": "Advisory",
@@ -72,65 +131,51 @@ export let periods = [
     "name": "1st Period",
     "time": [
       "08:10",
-      "08:40"
-    ]
-  },
-  {
-    "name": "2nd Period",
-    "time": [
-      "08:45",
-      "09:15"
+      "09:30"
     ]
   },
   {
     "name": "Break/Flex",
     "time": [
-      "09:20",
-      "09:30"
+      "09:35",
+      "09:55"
     ]
   },
   {
     "name": "3rd Period",
     "time": [
-      "9:35",
-      "10:05"
-    ]
-  },
-  {
-    "name": "4th Period",
-    "time": [
-      "10:10",
-      "10:40"
+      "10:00",
+      "11:20"
     ]
   },
   {
     "name": "5th Period",
     "time": [
-      "10:45",
-      "11:15"
+      "11:25",
+      "12:45"
     ]
   },
   {
-    "name": "Us Lunch",
+    "name": "US Lunch",
     "time": [
-      "11:20",
-      "11:50"
-    ]
-  },
-  {
-    "name": "6th Period",
-    "time": [
-      "11:55",
-      "12:25"
+      "12:50",
+      "13:30"
     ]
   },
   {
     "name": "7th Period",
     "time": [
-      "12:30",
-      "18:00"
+      "13:35",
+      "14:55"
+    ]
+  },
+  {
+    "name": "Study Hall",
+    "time": [
+      "14:55",
+      "15:15"
     ]
   }
 ]
 
-export let weekSchedule = ["B","C","B","C","A"]
+export let weekSchedule = ["N","B","C","A","A"]
