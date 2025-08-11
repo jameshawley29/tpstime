1c8f1b7865bb234199f46fcbaa329342236cc7bf
jameshawley29 <jameshawley29@gmail.com>
2025-08-10 23:00:20 -0400

    quick reachable fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,9 +7,6 @@ export function getTodayIndex(): number {
   // JS: Sun=0, Mon=1, Tue=2, Wed=3, Thu=4, Fri=5, Sat=6
   // Our: Mon=0, Tue=1, Wed=2, Thu=3, Fri=4
 
-  // delete after testing week behavior
-  return 2;
-
   if (jsDay >= 1 && jsDay <= 5) {
     return jsDay - 1;
   }
